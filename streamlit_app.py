ffb368dcec3c3e6f0784f3c1f4dca2140f46716a
zingith <121964868+zingith@users.noreply.github.com>
2023-01-04 17:54:48 +0100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 
 import streamlit
-
-streamlit.title("My first app")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
