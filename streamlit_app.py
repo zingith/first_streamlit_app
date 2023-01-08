64e8eec72d94a63f250adf2d41d127b44d314c6e
zingith <121964868+zingith@users.noreply.github.com>
2023-01-08 16:47:44 +0100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urlib.error import URLError
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
