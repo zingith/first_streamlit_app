0210655264f27c7132bca8dc093fd7b5d9ec9504
zingith <121964868+zingith@users.noreply.github.com>
2023-01-08 14:24:06 +0100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
